3e97819aad374886c6b66bdb4f0e14b29dfdc2ac
Will <wor101@gmail.com>
2021-04-16 16:45:28 -0400

    added URL notes

--- todo.rb
+++ todo.rb
@@ -15,15 +15,23 @@ get "/" do
   redirect "/lists"
 end
 
+# GET /lists        -> view all lists
+# GET /lists/new    -> new list form
+# POST /lists       -> create new list
+# GET /lists/1      -> view a single list
+
+# View list of lists
 get "/lists" do
   @lists = session[:lists] 
   erb :lists, layout: :layout
 end
 
+# Render the new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Create a new list
 post "/lists" do
   session[:lists] << {name: params[:list_name], todos: []}
   redirect "/lists"
