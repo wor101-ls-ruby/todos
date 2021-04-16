7a585828c4ac0a879621439441a78ee99c250c09
Will <wor101@gmail.com>
2021-04-16 16:39:20 -0400

    implementing list adding functionality

--- todo.rb
+++ todo.rb
@@ -2,14 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    {name: "Lunch Groceries", todos: []},
-    {name: "Dinner Groceries", todos: []}
-  ]
+  @lists = session[:lists] 
   erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+post "/lists" do
+  session[:lists] << {name: params[:list_name], todos: []}
+  redirect "/lists"
 end
