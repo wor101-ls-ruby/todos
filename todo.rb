7ca42306d04a83974a86d0975927251edeff8ba9
Will <wor101@gmail.com>
2021-04-17 17:44:28 +0000

    list name validation implemented

--- todo.rb
+++ todo.rb
@@ -1,6 +1,6 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
 configure do
   enable :sessions
@@ -11,8 +11,8 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
 # GET /lists        -> view all lists
@@ -21,18 +21,43 @@ end
 # GET /lists/1      -> view a single list
 
 # View list of lists
-get "/lists" do
-  @lists = session[:lists] 
+get '/lists' do
+  @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
 # Render the new list form
-get "/lists/new" do
+get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
+# Return an error message if the name is invalid
+def error_for_list_name(name)
+  if !(1..100).cover?(name.size)
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique.'
+  end
+end
+
 # Create a new list
-post "/lists" do
-  session[:lists] << {name: params[:list_name], todos: []}
-  redirect "/lists"
+post '/lists' do
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
+end
+
+  
+# View a single list
+get '/lists/:number' do
+
+  erb :list, layout: :layout
 end
